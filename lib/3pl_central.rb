da4b6e270e54efc15af606f9e821272f20d5931f
Adam Runner <adamrunner@gmail.com>
2014-12-01 14:06:33 -0800

    added Client class for ease of switching customers

--- lib/3pl_central.rb
+++ lib/3pl_central.rb
@@ -1,6 +1,7 @@
 require "3pl_central/version"
 require "3pl_central/configuration"
 require "3pl_central/base"
+require "3pl_central/client"
 require "3pl_central/order"
 require "3pl_central/services"
 require "3pl_central/stock_status"
@@ -14,5 +15,24 @@ end
 class Object
   def arrayify
     self.is_a?(Array) ? self : [self]
+  end
+end
+## String Extension
+class String
+  def camelize
+    self.split("_").each {|s| s.capitalize! }.join("")
+  end
+  # def camelize!
+  #   self.replace(self.split("_").each {|s| s.capitalize! }.join(""))
+  # end
+  def underscore
+    self.scan(/[A-Z][a-z]*/).join("_").downcase
+  end
+  # def underscore!
+  #   self.replace(self.scan(/[A-Z][a-z]*/).join("_").downcase)
+  # end
+
+  def constantize
+    Object.const_get(self)
   end
 end
---
+++ lib/3pl_central/client.rb
@@ -0,0 +1,33 @@
+module ThreePLCentral
+  class Client
+    def initialize(params)
+      ThreePLCentral.configure do |c|
+        c.three_pl_key = params[:three_pl_key]
+        c.login        = params[:login]
+        c.password     = params[:password]
+        c.three_pl_id  = params[:three_pl_id]
+      end
+    end
+
+    def method_missing(meth, *args)
+      if meth.to_s =~ /(create|find|get)_(.+)/
+        run_method($2, $1, *args)
+      else
+        super
+      end
+    end
+
+    def respond_to?(meth)
+      if meth.to_s =~ /(create|find|get)_(.+)/
+        true
+      else
+        super
+      end
+    end
+
+    def run_method(class_name, method_name, *args)
+      full_class = "ThreePLCentral::" + class_name.camelize
+      full_class.constantize.send(method_name.to_sym, *args)
+    end
+  end
+end
