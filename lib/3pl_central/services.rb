b91943172978d1fabff9d304e14e06d2bef89098
Adam Runner <adamrunner@gmail.com>
2014-12-05 12:41:03 -0800

    bugfix for creating orders

--- lib/3pl_central/services.rb
+++ lib/3pl_central/services.rb
@@ -34,41 +34,41 @@ module ThreePLCentral
                       end
                       xml.tns :PhoneNumber1, data[:ship_to][:phone_number1]
                       xml.tns :EmailAddress1, data[:ship_to][:email_address1]
-                      xml.tns :ShippingInstructions do |xml|
-                        xml.tns :Carrier, data[:ship_to][:shipping_instructions][:carrier]
-                        xml.tns :Mode, data[:ship_to][:shipping_instructions][:mode]
-                        xml.tns :ShippingNotes, data[:ship_to][:shipping_instructions][:shipping_notes]
-                      end
-                      xml.tns :Notes, data[:notes]
-                      xml.tns :OrderLineItems do |xml|
-                        data[:order_line_items].each do |order_item|
-                          xml.tns :OrderLineItem do |xml|
-                            xml.tns :Sku, order_item[:sku]
-                            xml.tns :Qty, order_item[:qty]
-                            xml.tns :FulfillmentSalePrice, order_item[:fulfillment_sale_price]
-                            xml.tns :FulfillmentDiscountPercentage, order_item[:fulfillment_discount_percentage]
-                            xml.tns :FulfillmentDiscountAmount, order_item[:fulfillment_discount_amount]
-                          end
+                    end
+                    xml.tns :ShippingInstructions do |xml|
+                      xml.tns :Carrier, data[:shipping_instructions][:carrier]
+                      xml.tns :Mode, data[:shipping_instructions][:mode]
+                      xml.tns :ShippingNotes, data[:shipping_instructions][:shipping_notes]
+                    end
+                    xml.tns :Notes, data[:notes]
+                    xml.tns :OrderLineItems do |xml|
+                      data[:order_line_items].each do |order_item|
+                        xml.tns :OrderLineItem do |xml|
+                          xml.tns :SKU, order_item[:sku]
+                          xml.tns :Qty, order_item[:qty]
+                          xml.tns :FulfillmentSalePrice, order_item[:fulfillment_sale_price]
+                          xml.tns :FulfillmentDiscountPercentage, order_item[:fulfillment_discount_percentage]
+                          xml.tns :FulfillmentDiscountAmount, order_item[:fulfillment_discount_amount]
                         end
                       end
-                      xml.tns :FulfillmentInfo do |xml|
-                        xml.tns :FulfillInvShippingAndHandling, data[:fulfillment_info][:fulfill_inv_shipping_and_handling]
-                        xml.tns :FulfillInvTax, data[:fulfillment_info][:fulfill_inv_tax]
+                    end
+                    xml.tns :FulfillmentInfo do |xml|
+                      xml.tns :FulfillInvShippingAndHandling, data[:fulfillment_info][:fulfill_inv_shipping_and_handling]
+                      xml.tns :FulfillInvTax, data[:fulfillment_info][:fulfill_inv_tax]
+                    end
+                    xml.tns :SoldTo do |xml|
+                      xml.tns :Name, data[:sold_to][:name]
+                      xml.tns :CompanyName, data[:sold_to][:company_name]
+                      xml.tns :Address do |xml|
+                        xml.tns :Address1, data[:sold_to][:address][:address1]
+                        xml.tns :Address2, data[:sold_to][:address][:address2]
+                        xml.tns :City, data[:sold_to][:address][:city]
+                        xml.tns :State, data[:sold_to][:address][:state]
+                        xml.tns :Zip, data[:sold_to][:address][:zip]
+                        xml.tns :Country, data[:sold_to][:address][:country]
                       end
-                      xml.tns :SoldTo do |xml|
-                        xml.tns :Name, data[:sold_to][:name]
-                        xml.tns :CompanyName, data[:sold_to][:company_name]
-                        xml.tns :Address do |xml|
-                          xml.tns :Address1, data[:sold_to][:address][:address1]
-                          xml.tns :Address2, data[:sold_to][:address][:address2]
-                          xml.tns :City, data[:sold_to][:address][:city]
-                          xml.tns :State, data[:sold_to][:address][:state]
-                          xml.tns :Zip, data[:sold_to][:address][:zip]
-                          xml.tns :Country, data[:sold_to][:address][:country]
-                        end
-                        xml.tns :PhoneNumber1, data[:sold_to][:phone_number1]
-                        xml.tns :EmailAddress1, data[:sold_to][:email_address1]
-                      end
+                      xml.tns :PhoneNumber1, data[:sold_to][:phone_number1]
+                      xml.tns :EmailAddress1, data[:sold_to][:email_address1]
                     end
                   end
                 end
