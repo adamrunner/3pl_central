282535d90086d3b235837d9863cc74faf5e1611b
Adam Runner <adamrunner@gmail.com>
2014-12-05 15:27:21 -0800

    put extensions in proper namespace

--- lib/3pl_central.rb
+++ lib/3pl_central.rb
@@ -7,32 +7,25 @@ require "3pl_central/services"
 require "3pl_central/stock_status"
 require "3pl_central/small_parcel_order"
 module ThreePLCentral
-  # Your code goes here...
-end
 
-## Object Extension
+  class Object
+    def arrayify
+      self.is_a?(Array) ? self : [self]
+    end
+  end
+  # Borrowed some implementations and conventions from Rails.
+  class String
+    def camelize
+      self.split("_").each {|s| s.capitalize! }.join("")
+    end
 
-class Object
-  def arrayify
-    self.is_a?(Array) ? self : [self]
-  end
-end
-## String Extension
-class String
-  def camelize
-    self.split("_").each {|s| s.capitalize! }.join("")
-  end
-  # def camelize!
-  #   self.replace(self.split("_").each {|s| s.capitalize! }.join(""))
-  # end
-  def underscore
-    self.scan(/[A-Z][a-z]*/).join("_").downcase
-  end
-  # def underscore!
-  #   self.replace(self.scan(/[A-Z][a-z]*/).join("_").downcase)
-  # end
+    def underscore
+      self.scan(/[A-Z][a-z]*/).join("_").downcase
+    end
 
-  def constantize
-    Object.const_get(self)
+
+    def constantize
+      Object.const_get(self)
+    end
   end
 end
